fa74ff23686ae2217a8ed4bddb88ecbc9d39e000
Bhavya <bhavyas3451@gmail.com>
2026-06-07 22:46:04 +0530

    again fixing utils

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api';
-
+const API_BASE = import.meta.env.VITE_API_BASE;
 const getToken = () => localStorage.getItem('expense_token');
 const saveToken = (token) => localStorage.setItem('expense_token', token);
 const clearToken = () => localStorage.removeItem('expense_token');
